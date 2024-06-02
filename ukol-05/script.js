9abab44c16619b07daf95d81b14cb2af48c11d86
Veronika Kolaříková <veronika.kolarikova@gnj.cz>
2024-06-02 19:00:35 +0200

    07

--- ukol-05/script.js
+++ ukol-05/script.js
@@ -26,3 +26,36 @@ selectPlan(1)
 selectPlan(2)
 selectPlan(3)
 selectPlan(1)
+
+
+/* Verze s Kubovou radou */
+
+/*
+
+const selectPlan = (planNumber) => {
+    const elPlanNumber = document.querySelector(`#plan${planNumber}`)
+
+    const plan1 = document.querySelector('#plan1')
+    const plan2 = document.querySelector('#plan2')
+    const plan3 = document.querySelector('#plan3')
+
+    plan1.classList.remove('plan--selected')
+    plan2.classList.remove('plan--selected')
+    plan3.classList.remove('plan--selected')
+
+        if(planNumber === 1) {
+            return elPlanNumber.classList.add('plan--selected')
+
+        } else if(planNumber === 2) {
+            return elPlanNumber.classList.add('plan--selected')
+
+        } else if(planNumber === 3) {
+            return elPlanNumber.classList.add('plan--selected')
+        }
+}
+
+selectPlan(1)
+selectPlan(2)
+selectPlan(3)
+selectPlan(2)
+*/
