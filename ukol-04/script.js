a874d6a9199ac7e2878f436d13c02b30c1718408
Veronika Kolaříková <veronika.kolarikova@gnj.cz>
2024-06-02 18:50:47 +0200

    11

--- ukol-04/script.js
+++ ukol-04/script.js
@@ -22,3 +22,24 @@ console.log(max3(10, 5, 7))
 console.log(max3(4, 9, 2))
 console.log(max3(8, 8, 7))
 console.log(max3(3, 3, 3))
+
+
+/* Jednodušší varianta - podle Kubovy rady*/
+
+/*
+const max3 = (x, y, z) => {
+    if (x >= y && x >= z) {
+        return x;
+    } else if (y >= x && y >= z) {
+        return y;
+    } else {
+        return z;
+    }
+}
+
+console.log(max3(1, 2, 3))
+console.log(max3(10, 5, 7))
+console.log(max3(4, 9, 2))
+console.log(max3(8, 8, 7))
+console.log(max3(3, 3, 3))
+*/
---
+++ ukol-06/script.js
@@ -0,0 +1,41 @@
+/* ÚKOL 06 
+
+Catering
+Mějme tři cateringové společnosti dodávající občerstvení na různé akce. Jsou to
+
+Just Food: Dodává levné jídlo vyrobené z nepříliš kvalitních surovin.
+
+Your Mama: Dodává středně drahé jídlo vyrobené z lepších surovin.
+
+Flavour Haven: Dodává drahé jídlo z kvalitních surovin.
+
+Napište pro každou z těcho společností funkci s jedním parametrem, který udává, pro kolik lidí se catering objednává. Funkce pak vrátí řetězec obsahující zprávu o tom že catering byl dodán a za jakou cenu. Například
+
+Catering od Flavour Haven pro 100 lidí za 300000 Kč
+
+Vyzkoušjte si vaše funkce v konzoli. Poté napište funkci createEvent, která představuje vytvoření nějaké události s cateringem. Funkce na vstupu obdrží název události, počet lidí a funkci, pomocí které se má provést catering. Funkce createEvent pak vrátí zprávu ve smyslu
+
+Událost Inaugurace prezidenta s catering od Flavour Haven pro 100 lidí za 300000 Kč   */
+
+
+const justFoodFunct = (pocetLidi) => {
+    return `cateringem od Just Food pro ${pocetLidi} lidí za ${Math.round(pocetLidi *120)} Kč.`
+}
+
+const yourMamaFunct = (pocetLidi) => {
+    return `cateringem od Your Mama pro ${pocetLidi} lidí za ${Math.round(pocetLidi * 500)} Kč.`
+}
+
+const flavourHavenFunct = (pocetLidi) => {
+    return `cateringem od Flavour Heaven pro ${pocetLidi} lidí za ${Math.round(pocetLidi * 3000)} Kč.`
+}
+
+const createEvent = (nazevUdalosti, pocetLidi, catering) => {
+  return `Událost ${nazevUdalosti} s ${catering(pocetLidi)}`;
+}
+
+document.body.innerHTML += createEvent(
+  'Inaugurace prezidenta',
+  100,
+  flavourHavenFunct
+)
